9be9ac94864b292ce676b4f777220c35f8e31250
Daigoro Toyama <daigoro.toyama@raima.com>
2022-03-01 13:23:18 -0800

    Fixed an issue with dealing with arguments

--- vi.ps1
+++ vi.ps1
@@ -10,14 +10,17 @@ else
 }
 
 $Script:vi_exec = "$Script:vi_dir\gvim.exe"
-$Script:colour_command = '-c "colorscheme elflord"'
+$Script:colour_scheme = 'elflord'
+## $Script:colour_command = '-c "colorscheme elflord"'
 
 if ($Script:args.count -eq 0)
 {
     Start-Process "$Script:vi_exec" -ArgumentList @($Script:colour_command)
+    Start-Process "$Script:vi_exec" -ArgumentList "-c `"colorscheme $SCRIPT:colour_scheme`""
 }
 else
 {
     $Script:file_name = $args[0]
-    Start-Process "$Script:vi_exec" -ArgumentList @($Script:colour_command, $Script:file_name)
+    #Start-Process "$Script:vi_exec" -ArgumentList @($Script:colour_command, $Script:file_name)
+    Start-Process "$Script:vi_exec" -ArgumentList "-c `"colorscheme $SCRIPT:colour_scheme`"", "$SCRIPT:file_name"
 }

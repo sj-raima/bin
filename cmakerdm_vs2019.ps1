edf79e76cdeb9ed4534a392bf2102d7c2363a11b
Daigoro Toyama <daigoro.toyama@raima.com>
2022-03-10 14:13:50 -0800

    CMake build setup script for VS 2019
    Added command-line parameter handling

--- cmakerdm_vs2019.ps1
+++ cmakerdm_vs2019.ps1
@@ -2,19 +2,27 @@
 ## PowerShell script to set up the CMake build environment for VS 2019
 ###############################################################################
 
-if ($ENV:RDM_HOME -eq $null)
+$SCRIPT:BRANCH = 'milan'
+
+if ($args.count -eq 0)
 {
-    $SCRIPT:RDM_HOME = 'C:\Users\dtoyama\rdm\milan'
+    $SCRIPT:BRANCH = 'milan'
+}
+elseif ($args.count -eq 1)
+{
+    $SCRIPT:BRANCH = $args[0]
 }
 else
 {
-    $SCRIPT:RDM_HOME = $ENV:RDM_HOME
+    $Host.UI.WriteErrorLine("CMAKERDM only takes one argument")
+    exit 1
 }
+
+$SCRIPT:RDM_HOME = "C:\Users\dtoyama\rdm\$SCRIPT:BRANCH"
 
 $SCRIPT:S_BUILD_LOCATION='b-vs2019'
 $SCRIPT:S_CMAKE_COMPILER='Visual Studio 16 2019'
 $SCRIPT:S_ALL_SOURCE='Off'
-$SCRIPT:S_DYNAMIC_LIBS='ON'
 
 $SCRIPT:BUILD_PATH = (Join-Path -Path "$SCRIPT:RDM_HOME" -ChildPath "$SCRIPT:S_BUILD_LOCATION")
 
@@ -30,5 +38,5 @@ $SCRIPT:BUILD_BIN = (Join-Path -Path "$SCRIPT:BUILD_PATH" -ChildPath 'bin')
 $SCRIPT:BUILD_LIB = (Join-Path -Path "$SCRIPT:BUILD_PATH" -ChildPath 'lib')
 
 ## Start-Process cmake.exe -ArgumentList "-G `"$SCRIPT:S_CMAKE_COMPILER`" .."
-cmake.exe -G `""$SCRIPT:S_CMAKE_COMPILER"`" -DBUILD_SHARED_LIBS:BOOL="$SCRIPT:S_DYNAMIC_LIB" -DCOMPILE_ALL_SOURCE:BOOL="$SCRIPT_S_ALL_SOURCE" -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:STRING="$SCRIPT:BUILD_BIN" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="$SCRIPT:BUILD_LIB" ..
+cmake.exe -G `""$SCRIPT:S_CMAKE_COMPILER"`" -DBUILD_SHARED_LIBS:BOOL=ON -DCOMPILE_ALL_SOURCE:BOOL="$SCRIPT:S_ALL_SOURCE" -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:STRING="$SCRIPT:BUILD_BIN" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$SCRIPT:BUILD_LIB" ..
 

bbb41e57b1c6cb55a172df83d6061d566896e3c2
Daigoro Toyama <daigoro.toyama@raima.com>
2022-03-11 13:55:00 -0800

    Added parameter handling

--- cmakerdm_vs2019.ps1
+++ cmakerdm_vs2019.ps1
@@ -1,24 +1,12 @@
 ###############################################################################
 ## PowerShell script to set up the CMake build environment for VS 2019
 ###############################################################################
+param
+(
+    [String]$branch = 'milan'
+)
 
-$SCRIPT:BRANCH = 'milan'
-
-if ($args.count -eq 0)
-{
-    $SCRIPT:BRANCH = 'milan'
-}
-elseif ($args.count -eq 1)
-{
-    $SCRIPT:BRANCH = $args[0]
-}
-else
-{
-    $Host.UI.WriteErrorLine("CMAKERDM only takes one argument")
-    exit 1
-}
-
-$SCRIPT:RDM_HOME = "C:\Users\dtoyama\rdm\$SCRIPT:BRANCH"
+$SCRIPT:RDM_HOME = "C:\Users\dtoyama\rdm\$branch"
 
 $SCRIPT:S_BUILD_LOCATION='b-vs2019'
 $SCRIPT:S_CMAKE_COMPILER='Visual Studio 16 2019'
@@ -37,6 +25,5 @@ Set-Location "$SCRIPT:BUILD_PATH"
 $SCRIPT:BUILD_BIN = (Join-Path -Path "$SCRIPT:BUILD_PATH" -ChildPath 'bin')
 $SCRIPT:BUILD_LIB = (Join-Path -Path "$SCRIPT:BUILD_PATH" -ChildPath 'lib')
 
-## Start-Process cmake.exe -ArgumentList "-G `"$SCRIPT:S_CMAKE_COMPILER`" .."
 cmake.exe -G `""$SCRIPT:S_CMAKE_COMPILER"`" -DBUILD_SHARED_LIBS:BOOL=ON -DCOMPILE_ALL_SOURCE:BOOL="$SCRIPT:S_ALL_SOURCE" -DCMAKE_RUNTIME_OUTPUT_DIRECTORY:STRING="$SCRIPT:BUILD_BIN" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$SCRIPT:BUILD_LIB" ..
 
